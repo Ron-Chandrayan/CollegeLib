8cec547a44721297fa93bf137133b3f1d0b0b3bc
TAGISWILD <chauhan.a.atharvak26@gmail.com>
2025-07-23 21:21:03 +0530

    pushes via auto

--- backend/index.js
+++ backend/index.js
@@ -18,3 +18,14 @@ app.get('/', (req, res) => res.send('Hello World!'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+
+const members = require('./models/members');
+
+app.get('/fetch', async (req, res) => {
+  try {
+    const allmembers = await members.find().sort({ createdAt: 1 });
+    res.json(allmembers);
+  } catch (error) {
+    res.status(500).json({ error: "student data not found" });
+  }
+});
