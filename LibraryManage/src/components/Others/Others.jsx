80e67e653597a499daef8b0309fe21438b5240b3
Ron-Chandrayan <chandrayanpaul27@gmail.com>
2025-08-02 00:56:30 +0530

    updated the others page

--- LibraryManage/src/components/Others/Others.jsx
+++ LibraryManage/src/components/Others/Others.jsx
@@ -2,8 +2,66 @@ import React from 'react'
 
 function Others() {
   return (
-    <div>
-      <p>This be a miscellenous page</p>
+    <div><p>
+        Dear Library Users,
+
+Welcome to the GST Central Library Portal!
+
+This platform has been developed by the website development team to enhance your interaction with the GST Central Library. It is designed to provide easy access to library information, services, ongoing activities, and relevant academic resources.
+
+Our goal is to create a seamless and user-friendly experience that supports your academic and research needs.
+
+We welcome your suggestions and feedback regarding the portal and the library services. Your input is valuable and will help us continuously improve the system to better serve the GST community.
+
+— Developed with dedication by the Team
+</p>
+  Important Links
+      <a href="https://ndl.iitkgp.ac.in/">Link to National Digital Library</a>
+      <a href="https://swayam.gov.in/">Link to Swayam</a>
+      <a href="https://epgp.inflibnet.ac.in/">Link to E-Pathshala</a>
+      <a href="https://nptelvideos.com/">Link to NPTEL videos</a>
+      <a href="https://egyankosh.ac.in/handle/123456789/35033">Link to E-GyanKosh</a>
+      <a href="https://nad.gov.in/">Link to National Academy Repositories</a>
+      <a href="https://www.onlinenewspapers.com/india.shtml">E-newspaper</a>
+      <a href="https://mu.ac.in/syllabus#1548848043985-b224e6f7-a0de">Mumbai University Syllabus</a>
+      <a href="https://scholarships.gov.in/">Link to Scholarships</a>
+      <a href="https://upsc.gov.in/">UPSC</a>
+      <a href="http://gate.iitd.ac.in/">GATE</a>
+      <a href="https://www.ets.org/gre.html">GRE</a>
+      <a href="https://satsuite.collegeboard.org/sat">SAT</a>
+      <a href="https://www.ets.org/toefl.html">TOEFL</a>
+      <a href="https://www.aicte.gov.in/opportunities/students/research-funds">Link to Research Funds</a>
+      <div>General Rules:
+                    Students should maintain silence in the library at all the time. Group discussion is strictly not allowed.
+            Personal Books, Printed Materials, Bags & plastic carry bags are not allowed to bring inside the library.
+            Members are held responsible for all items issued on their name, until they are returned to the library in good condition.
+            The lost or mutilated book should be brought to the notice of library staff immediately.
+            If an issued book is lost / damaged, it is to be either replaced with latest edition or compensated by double the original cost at the prevailing exchange rate. Refunding compensation money may not be possible if book lost is found later.
+            The borrowing facility can be restricted or suspended in case of misbehavior or misuse of the library facilities / materials.
+            Use of mobile is strictly prohibited in and around the library premises.
+            Documents marked as “Reference Copy” are to be consulted within the library premises.
+            Materials issued out from the library are to be returned at the Circulation Desk only during its working hours before the due date mentioned.
+            A document issued may be renewed provided there is no pending reservation against it.
+            Borrower should bring the material physically to the library for renewal.
+            No user is eligible to borrow documents from the library on someone’s behalf.
+            No material from the library should be taken out without proper issue. Any type of violation may lead to a disciplinary action.
+
+      </div>
+      <div>Library Timings:
+                    Monday to Saturday: 08 am to 07 pm (11 hrs)
+
+            ​
+
+            During Vacation:  09 am to 5 pm (8 hrs)
+
+            ​
+
+            The library will remain closed on Sundays' &  Public holidays 
+
+            
+
+            Regarding extension of timings, if any, after 7 pm, contact at circulation counter or mail: librariangst@sies.edu.in 
+      </div>
     </div>
   )
 }
