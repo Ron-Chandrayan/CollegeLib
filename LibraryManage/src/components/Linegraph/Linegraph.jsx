12eccbb65c9e2cf751489ca760cae4c783017705
Ron-Chandrayan <chandrayanpaul27@gmail.com>
2025-08-03 00:12:16 +0530

    doing changes

--- LibraryManage/src/components/Linegraph/Linegraph.jsx
+++ LibraryManage/src/components/Linegraph/Linegraph.jsx
@@ -13,13 +13,18 @@ import {
 
 
 export default function Linegraph({data}) {
+    const formatTime = (timestamp) => {
+    const date = new Date(timestamp);
+    return date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+    // You can customize locale and options as needed
+  };
   return (
     <div className="w-full h-96 p-4 bg-white rounded shadow">
       <h2 className="text-xl font-semibold mb-4">Hourly Footfall</h2>
       <ResponsiveContainer width="100%" height="100%">
         <LineChart data={data} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
           <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="timestamp"  />
+          <XAxis dataKey="timestamp" tickFormatter={formatTime}  />
           <YAxis />
           <Tooltip />
           <Legend />
