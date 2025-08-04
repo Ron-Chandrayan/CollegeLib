58e70e046788bf1496d3bbb4c327da99cfe10be2
Ron-Chandrayan <chandrayanpaul27@gmail.com>
2025-08-05 00:18:53 +0530

    doing changes

--- backend/models/Users.js
+++ backend/models/Users.js
@@ -1,10 +1,19 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-  name:String,
-  PRN:String,
-  password:String
+  name: String,
+  PRN: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return /^[0-9]{3}[A-Za-z][A-Za-z0-9][0-9]{4}$/.test(v);
+      },
+      message: props => `${props.value} is not a valid PRN!`
+    }
+  },
+  password: String
 });
 
 const Users = mongoose.model('Users', UserSchema);
-module.exports=Users;
+module.exports = Users;
