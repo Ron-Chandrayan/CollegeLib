202858ebb6a184a77422d17d625774890e8dc3a5
Ron-Chandrayan <chandrayanpaul27@gmail.com>
2025-08-16 22:34:41 +0530

    step by step

--- backend/index.js
+++ backend/index.js
@@ -60,7 +60,7 @@ mongoose.connect(process.env.MONGO_URI, {
 
      const data = await lifetime.countDocuments({
       createdAt: { $gte: start, $lt: end }
-    }).sort({ createdAt: 1 });
+    });
 
     return data;
   
