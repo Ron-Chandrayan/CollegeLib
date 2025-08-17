35e6e86c055f5656c54c759a096cc82bf0d6f274
TAGISWILD <chauhan.a.atharvak26@gmail.com>
2025-08-17 18:30:05 +0530

    security hotfix 1 patched

--- backend/s3Config.js
+++ backend/s3Config.js
@@ -1,8 +1,8 @@
 const AWS = require('aws-sdk');
 
 const s3Config = {
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || '',
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   region: process.env.AWS_REGION || 'ap-south-1',
   bucketName: process.env.AWS_BUCKET_NAME || 'audacty-backend'
 };
