9f65b7985b8f3e36aa19997ed8a9b844b9515587
TAGISWILD <chauhan.a.atharvak26@gmail.com>
2025-08-18 20:24:54 +0530

    updated models

--- backend/models/FeStudent.js
+++ backend/models/FeStudent.js
@@ -2,9 +2,10 @@ const mongoose = require('mongoose');
 
 const feStudentSchema = new mongoose.Schema({
   PRN: { type: String, required: true, unique: true },
-  batch: { type: String, required: true },
+  batch: { type: String, required: false },
   name: { type: String, required: true },
-  division: { type: String, required: true },
+  division: { type: String, required: false },
+  email: { type: String, required: true },
 });
 
 const FeStudent = mongoose.model('fe_students', feStudentSchema);
