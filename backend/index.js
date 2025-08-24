a3bd941602407c8f94409970324ec94ac6082c81
Ron-Chandrayan <chandrayanpaul27@gmail.com>
2025-08-24 11:35:00 +0530

    storing time logs

--- backend/index.js
+++ backend/index.js
@@ -80,9 +80,9 @@ mongoose.connect(process.env.MONGO_URI, {
  }
 
 async function difference(start,end){
-   startime=new Date(start);
-   endtime= new Date(end);
-   const diff = endtime-starttime;
+  const  startime=new Date(start);
+  const endtime= new Date(end);
+   const diff = endtime-startime;
    const diffinsec=Math.floor(diff/1000);
   //  console.log(diffinsec);
    return diffinsec;
