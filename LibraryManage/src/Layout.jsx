efb1e4c56d1b39d01967774b1ac21ef2893215ca
Ron-Chandrayan <chandrayanpaul27@gmail.com>
2025-08-25 21:01:02 +0530

    creating a new view

--- LibraryManage/src/Layout.jsx
+++ LibraryManage/src/Layout.jsx
@@ -238,7 +238,7 @@ function Layout() {
 
   return (
     <div>
-      {front?<Header signup={signup} setSignup={setSignup} login={login} setLogin={setLogin} library={library} time={time} settime={settime} timer={timer} settimer={settimer} front={front}/>:null}
+      {front?null:<Header signup={signup} setSignup={setSignup} login={login} setLogin={setLogin} library={library} time={time} settime={settime} timer={timer} settimer={settimer} front={front}/>}
        
        <Outlet context={{name,setName,todayfootfall,settodayfootfall,totalfootfall,settotalfootfall,Students,setStudents,loading,setloading,signup,setSignup,login,setLogin,welcome,setwelcome, books,setBooks,universalformData,setuniversalFormData,welcome2,setwelcome2,info, setInfo,info2, setInfo2,library,setlibrary,formData,setFormData,time,settime,timer,settimer,front,setfront}} />
        <Footer/>
