966dab8478df7ca948cc0ff98e835fdaf06d9905
Ron-Chandrayan <chandrayanpaul27@gmail.com>
2025-08-27 11:14:18 +0530

    changing UI

--- LibraryManage/src/components/Library/Library.jsx
+++ LibraryManage/src/components/Library/Library.jsx
@@ -563,7 +563,7 @@ function Library() {
       </div>  
 
       {/* Main Content Grid */}
-      <div className="grid grid-cols-12 gap-6">
+      <div className="bg-gradient-to-br from-slate-900 to-gray-500 grid grid-cols-12 gap-6">
         
         {/* Current Students Card - Smaller */}
         <div className="col-span-2">
